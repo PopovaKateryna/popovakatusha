fcca80236767005fb16f98e75edbc7ab43957aa1
popovakatusha <kateryna.popova.it.2021@lpnu.ua>
2021-09-15 15:07:35 +0300

    add pr

---
+++ pr2/pr2/pr2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double z1;
+	double z2;
+
+	cout << "a = ";
+	cin >> a;
+
+	z1 = pow((((1 + a + pow(a,2)) / (2 * a + pow(a,2))) + 2) - ((1 - a + pow(a,2)) / (2 * a - pow(a,2))),-1) * (5 - (2 * pow(a,2)));
+	z2 = ((4 - pow(a, 2)) / 2);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
